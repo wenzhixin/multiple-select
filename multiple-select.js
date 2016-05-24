3f864e1240a1d3d2ef014224deead60e6d79d954
Haochi Kiang <hckiang@riseup.net>
2016-05-24 13:26:45 +0800

    New option for group checking when filtered
    Implementation of issue discussed in this issue:
    https://github.com/wenzhixin/multiple-select/issues/272

--- multiple-select.js
+++ multiple-select.js
@@ -367,7 +367,9 @@
             });
             this.$selectGroups.off('click').on('click', function () {
                 var group = $(this).parent().attr('data-group'),
-                    $items = that.$selectItems.filter(':visible'),
+                    $items = that.options.multipleSelectFilteredOnly
+                               ? that.$selectItems.filter(':visible')
+                               : that.$selectItems,
                     $children = $items.filter(sprintf('[data-group="%s"]', group)),
                     checked = $children.length !== $children.filter(':checked').length;
 
@@ -735,6 +737,7 @@
         addTitle: false,
         filterAcceptOnEnter: false,
         hideOptgroupCheckboxes: false,
+        multipleSelectFilteredOnly: true,
 
         selectAllText: 'Select all',
         allSelected: 'All selected',
