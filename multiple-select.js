f28b7ae8ff88165c604cc7983254e83b160b7924
Oleksii Mylotskyi <oleksii.mylotskyi@milsdev.com>
2016-08-06 15:36:47 +0300

    Add passibilities to load via AMD compatible way.

--- multiple-select.js
+++ multiple-select.js
@@ -5,7 +5,15 @@
  * http://wenzhixin.net.cn/p/multiple-select/
  */
 
-(function ($) {
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module depending on jQuery.
+        define(['jquery'], factory);
+    } else {
+        // No AMD. Register plugin with global jQuery object.
+        factory(jQuery);
+    }
+}(function ($) {
 
     'use strict';
 
@@ -779,4 +787,4 @@
             return false;
         }
     };
-})(jQuery);
+}));
