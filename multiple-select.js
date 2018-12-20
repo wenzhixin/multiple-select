f1be42aa13af297273863cf27cbe321fdc447b4c
文翼 <wenzhixin2010@gmail.com>
2018-12-20 08:35:31 +0800

    Update multiple-select.js

--- multiple-select.js
+++ multiple-select.js
@@ -474,7 +474,7 @@
 
         update: function (isInit) {
            	// allow placeholder taken from html 
-	    this.options.placeholder = this.options.placeholder ? this.options.placeholder : this.$el.attr("placeholder");
+	    this.options.placeholder = this.options.placeholder ? this.options.placeholder : this.$el.attr('placeholder');
             var selects = this.options.displayValues ? this.getSelects() : this.getSelects('text'),
                 $span = this.$choice.find('>span'),
                 sl = selects.length;
