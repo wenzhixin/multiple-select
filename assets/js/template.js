85193e9adb886359740aedac5fda8c95f01bc355
Travis CI <wenzhixin2010@gmail.com>
2019-05-15 23:32:10 +0000

    Deploy to GitHub Pages: 93894b43e3ceddda9ea30aac5c4bfdacfda6da9c

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,8 +1,8 @@
 $(function () {
-  var url = location.search.replace(/\?v=\d+&/, '').replace(/\?v=155&/, '')
+  var url = location.search.replace(/\?v=\d+&/, '').replace(/\?v=156&/, '')
   $.ajax({
     type: 'GET',
-    url: url + '?v=155', // todo: add version to solve cache problem
+    url: url + '?v=156', // todo: add version to solve cache problem
     dataType: 'html',
     global: false,
     cache: true, // (warning: setting it to false will cause a timestamp and will call the request twice)
