828b7bddb21e035551a32854f74fd2721c70aa74
文翼 <wenzhixin2010@gmail.com>
2019-10-26 16:33:21 +0800

    Update multiple-select-it-IT.js

--- src/locale/multiple-select-it-IT.js
+++ src/locale/multiple-select-it-IT.js
@@ -1,30 +1,21 @@
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('jquery')) :
-  typeof define === 'function' && define.amd ? define(['jquery'], factory) :
-  (global = global || self, factory(global.jQuery));
-}(this, function ($) { 'use strict';
+/**
+ * Multiple Select it-IT translation
+ * Author: Giuseppe Lodi Rizzini
+ */
 
-  $ = $ && $.hasOwnProperty('default') ? $['default'] : $;
+$.fn.multipleSelect.locales['it-IT'] = {
+  formatSelectAll () {
+    return '[Seleziona tutti]'
+  },
+  formatAllSelected () {
+    return 'Tutti selezionati'
+  },
+  formatCountSelected (count, total) {
+    return count + ' di ' + total + ' selezionati'
+  },
+  formatNoMatchesFound () {
+    return 'Nessun risultato'
+  }
+}
 
-  /**
-   * Multiple Select it-IT translation
-   * Author: Giuseppe Lodi Rizzini
-   */
-
-  $.fn.multipleSelect.locales['it-IT'] = {
-    formatSelectAll: function formatSelectAll() {
-      return '[Seleziona tutti]';
-    },
-    formatAllSelected: function formatAllSelected() {
-      return 'Tutti selezionati';
-    },
-    formatCountSelected: function formatCountSelected(count, total) {
-      return count + ' di ' + total + ' selezionati';
-    },
-    formatNoMatchesFound: function formatNoMatchesFound() {
-      return 'Nessun risultato';
-    }
-  };
-  $.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['it-IT']);
-
-}));
+$.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['it-IT'])
