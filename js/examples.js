bcf874a1de00597302d0bb3c96afd8df710032a3
Travis CI <wenzhixin2010@gmail.com>
2019-11-05 06:41:42 +0000

    Deploy to GitHub Pages: b33f8cf0ead5e3e8e1ae8e73dfa5672ac57b0632

--- js/examples.js
+++ js/examples.js
@@ -16,12 +16,12 @@ function initUrl() {
 function loadUrl(url_) {
   var template = 'templates/template.html'
 
-  var url = template + '?v=185&url=' + url_
+  var url = template + '?v=186&url=' + url_
   if (window._config.isDebug) {
     url = template + '?t=' + (+new Date()) + '&url=' + url_
   }
   if (window._config.isViewSource) {
-    url = template + '?v=185&view-source&url=' + url_ + '#view-source'
+    url = template + '?v=186&view-source&url=' + url_ + '#view-source'
   }
   $('iframe').attr('src', url)
 
