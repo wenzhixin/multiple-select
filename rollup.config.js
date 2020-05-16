38e8ec31426423186621d336ab17feaf98ed2951
Brett Zamir <brettz9@yahoo.com>
2020-05-16 10:44:17 +0800

    - Build: Use "json" extension with babelrc - Maintenance: Add `.editorconfig` for setting indents in editors before-the-fact - npm: Switch to maintained `@rollup/plugin-babel` - npm: Avoid vulnerability by patching node-sass

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import glob from 'glob'
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
