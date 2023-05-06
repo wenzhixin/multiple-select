162e88cc3f840328215ac3a2fb9708d241f8504d
HThuren <thuren.henrik@gmail.com>
2023-05-06 23:31:10 +0200

    add pt-BR and cz-CS

--- dist/multiple-select-locale-all.js
+++ dist/multiple-select-locale-all.js
@@ -6,28 +6,49 @@
 
   $ = $ && $.hasOwnProperty('default') ? $['default'] : $;
 
-  /**
-   * Multiple Select en-US translation
-   * Author: Zhixin Wen<wenzhixin2010@gmail.com>
-   */
+/**
+ * Multiple Select cz-CS translation
+ * Author: Matej Puhony<info@puhony.eu>
+ */
 
-  $.fn.multipleSelect.locales['en-US'] = {
-    formatSelectAll: function formatSelectAll() {
-      return '[Select all]';
-    },
-    formatAllSelected: function formatAllSelected() {
-      return 'All selected';
-    },
-    formatCountSelected: function formatCountSelected(count, total) {
-      return count + ' of ' + total + ' selected';
-    },
-    formatNoMatchesFound: function formatNoMatchesFound() {
-      return 'No matches found';
-    }
-  };
-  $.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['en-US']);
+$.fn.multipleSelect.locales['cz-CS'] = {
+  formatSelectAll () {
+    return '[Vybrat vše]'
+  },
+  formatAllSelected () {
+    return 'Vše vybráno'
+  },
+  formatCountSelected (count, total) {
+    return count + ' z ' + total + ' vybráno'
+  },
+  formatNoMatchesFound () {
+    return 'Nebylo nalezeno'
+  }
+}
+$.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['cz-CS'])
 
-  /**
+/**
+ * Multiple Select da-DK translation
+ * Author: HThuren<thuren.henrik@gmail.com>
+ */
+
+$.fn.multipleSelect.locales['da-DK'] = {
+  formatSelectAll () {
+    return '[Vælg alle]'
+  },
+  formatAllSelected () {
+    return 'Alle valgt'
+  },
+  formatCountSelected (count, total) {
+    return count + ' af ' + total + ' valgt'
+  },
+  formatNoMatchesFound () {
+    return 'Søgning uden resultat'
+  }
+}
+$.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['da-DK'])
+
+/**
    * Multiple Select es-ES translation
    * Author: Zhixin Wen<wenzhixin2010@gmail.com>
    */
@@ -47,6 +68,48 @@
     }
   };
   $.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['es-ES']);
+
+/**
+ * Multiple Select fr-FR translation
+ * Author: Francis Perron <francis@hivetek.com>
+ */
+
+$.fn.multipleSelect.locales['fr-FR'] = {
+  formatSelectAll () {
+    return '[Tout sélectionner]'
+  },
+  formatAllSelected () {
+    return 'Tous sélectionnés'
+  },
+  formatCountSelected (count, total) {
+    return count + ' de ' + total + ' sélectionnés'
+  },
+  formatNoMatchesFound () {
+    return 'Aucun résultat'
+  }
+}
+$.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['fr-FR'])
+
+/**
+ * Multiple Select hu-HU translation
+ * Author: Péter Báthory<bathory86p@gmail.com>
+ */
+
+$.fn.multipleSelect.locales['hu-HU'] = {
+  formatSelectAll () {
+    return '[Összes kiválasztása]'
+  },
+  formatAllSelected () {
+    return 'Összes kiválasztva'
+  },
+  formatCountSelected (count, total) {
+    return count + ' / ' + total + ' kiválasztva'
+  },
+  formatNoMatchesFound () {
+    return 'Nincs találat'
+  }
+}
+$.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['hu-HU'])
 
   /**
    * Multiple Select it-IT translation
@@ -68,6 +131,69 @@
     }
   };
   $.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['it-IT']);
+
+  /**
+ * Multiple Select pt-BR translation
+ * Author: Lucas Aguiar<lucas99.abreu@gmail.com>
+ */
+
+$.fn.multipleSelect.locales['pt-BR'] = {
+  formatSelectAll () {
+    return '[Selecionar todos]'
+  },
+  formatAllSelected () {
+    return 'Todos selecionados'
+  },
+  formatCountSelected (count, total) {
+    return count + ' de ' + total + ' selecionado(s)';
+  },
+  formatNoMatchesFound () {
+    return 'Nenhum resultado encontrado'
+  }
+}
+$.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['pt-BR'])
+
+/**
+ * Multiple Select ru-RU translation
+ * Author: Zhixin Wen<wenzhixin2010@gmail.com>
+ */
+
+$.fn.multipleSelect.locales['ru-RU'] = {
+  formatSelectAll () {
+    return '[Выбрать все]'
+  },
+  formatAllSelected () {
+    return 'Выбрано все'
+  },
+  formatCountSelected (count, total) {
+    return count + ' из ' + total + ' выбрано'
+  },
+  formatNoMatchesFound () {
+    return 'Совпадений не найдено'
+  }
+}
+$.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['ru-RU'])
+
+/**
+ * Multiple Select vi-VN translation
+ * Author: hoangbaovu <ineo.vn>
+ */
+
+$.fn.multipleSelect.locales['vi-VN'] = {
+  formatSelectAll () {
+    return '[Tất cả]'
+  },
+  formatAllSelected () {
+    return 'Chọn tất cả'
+  },
+  formatCountSelected (count, total) {
+    return 'Đã chọn ' + count + ' trong ' + total
+  },
+  formatNoMatchesFound () {
+    return 'Không tìm thấy kết quả.'
+  }
+}
+$.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['vi-VN'])
 
   /**
    * Multiple Select zh-CN translation
@@ -111,4 +237,25 @@
   };
   $.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['zh-TW']);
 
+    /**
+   * Multiple Select en-US translation
+   * Author: Zhixin Wen<wenzhixin2010@gmail.com>
+   */
+
+    $.fn.multipleSelect.locales['en-US'] = {
+      formatSelectAll: function formatSelectAll() {
+        return '[Select all]';
+      },
+      formatAllSelected: function formatAllSelected() {
+        return 'All selected';
+      },
+      formatCountSelected: function formatCountSelected(count, total) {
+        return count + ' of ' + total + ' selected';
+      },
+      formatNoMatchesFound: function formatNoMatchesFound() {
+        return 'No matches found';
+      }
+    };
+    $.extend($.fn.multipleSelect.defaults, $.fn.multipleSelect.locales['en-US']);
+    
 }));
